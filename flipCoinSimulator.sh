4dd4f2bdb5694d439d0c2d6e3e2c9707b80877f8
WassiW80 <sayedwaseeyat111@gmail.com>
2020-01-25 12:45:22 +0530

    ADDED: Percentage calculation of singlet combination.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,22 @@
 #!/bin/bash -x
-randomValue=$((RANDOM%2))
-if [ $randomValue -eq 1 ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+declare -A coinsDictionary
+read -p "Enter how times you want to flip coin: " countOfCoin
+
+function flipCoin(){
+for((counter=0;counter<$countOfCoin;counter++))
+	do
+		randomValue=$((RANDOM%2))
+		if [ $randomValue -eq 1 ]
+		then
+			 coinsDictionary[H]=$((++head))
+		else
+			 coinsDictionary[T]=$((++tail))
+		fi
+	done
+	echo "Key :	 	"${!coinsDictionary[@]}
+	echo "Counts :	"${coinsDictionary[@]}
+	headPercentage=`echo "scale=2; ${coinsDictionary[H]}*100/$countOfCoin" | bc `
+	tailPercentage=`echo "scale=2; ${coinsDictionary[T]}*100/$countOfCoin" | bc `
+	echo "Percentage : " $headPercentage% $tailPercentage%
+}
+flipCoin
